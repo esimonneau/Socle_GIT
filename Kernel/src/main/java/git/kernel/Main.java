4e1c0d21a3ee4f1ff5fac53e5cfd1fa509f4299b
Emeric SIMONNEAU <esimonneau@generixgroup.com>
2013-01-07 16:27:12 +0100

    Ajout de Main

---
+++ Kernel/src/main/java/git/kernel/Main.java
@@ -0,0 +1,20 @@
+package git.kernel;
+
+/**
+ * <p>
+ * </p>
+ *
+ * @author esimonneau
+ */
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args )
+	{
+
+	}
+
+}
